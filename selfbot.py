5518c04d98d94a85f83758ee8d5a5959d09da52c
Dropout <67494861+Dropout1337@users.noreply.github.com>
2020-07-10 01:01:30 +1200

    Create selfbot.py

---
+++ selfbot.py
@@ -0,0 +1,32 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+token = 'User Token Here'
+
+prefix = 'gay!'
+client = discord.Client()
+client = commands.Bot(
+    description='Dropout',
+    command_prefix=prefix,
+    self_bot=True
+)
+client.remove_command('help') 
+
+@client.event
+async def on_ready():
+    print('Online')
+   
+
+@client.command()
+async def dmall(ctx, *, message):
+    for user in list(ctx.guild.members):
+        try:
+            await asyncio.sleep(0.5)    
+            await user.send(message)
+            await ctx.send(f'Sent "{message}" To {user}')
+        except:
+            pass
+
+    
+client.run(token, bot = False)
