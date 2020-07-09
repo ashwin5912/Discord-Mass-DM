e407f3a20b3535bdd6833d573787703106413ff9
Dropout <67494861+Dropout1337@users.noreply.github.com>
2020-07-10 00:59:02 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-import discord, asyncio
+import discord
+import asyncio
 from discord.ext import commands
 
-token = 'Your Bots Token Here'
+token = 'NzMwNTkxMjgxMzI3MjQzMzA1.XwcTWA.VwOMxyjDou0BBlXTu09xSODUIw4'
 
 client = commands.Bot(command_prefix = '++')
 client.remove_command('help')
 
-async def status():
-    while True:
-        watch = discord.Activity(type = discord.ActivityType.watching, name = 'Made By Dropout <3')
-        await client.change_presence(activity = watch)
-        await asyncio.sleep(10800)
-        
 @client.event
 async def on_ready():
     print('Online')
-    status()
    
 
 @client.command()
 async def dmall(ctx, *, message):
     for user in list(ctx.guild.members):
         try:
-            await asyncio.sleep(5)    
+            await asyncio.sleep(0.5)    
             await user.send(message)
             await ctx.send(f'Sent "{message}" To {user}')
         except:
